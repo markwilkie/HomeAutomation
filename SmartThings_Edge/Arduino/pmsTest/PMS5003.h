c149580727d0795c7a82aeefe099c71051c0511a
Mark Wilkie <public@wilkiefamily.net>
2022-08-07 19:47:18 -0700

    working PMS5003 sample

---
+++ SmartThings_Edge/Arduino/pmsTest/PMS5003.h
@@ -0,0 +1,42 @@
+#ifndef PMS5003_H
+#define PMS5003_H
+#include <Arduino.h>
+
+#define TIMEOUT     1000  //how long to wait for data before erroring in ms
+#define TOTALTRIES  10    //how often to try while waiting to timeout
+
+struct pms5003data 
+{
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+
+class PMS5003 
+{
+  public:
+    PMS5003();
+    bool readPMSData();
+
+    uint16_t getpm10_standard();
+    uint16_t getpm25_standard();
+    uint16_t getpm100_standard();
+    
+    uint16_t getpm10_env();
+    uint16_t getpm25_env();
+    uint16_t getpm100_env();
+
+    uint16_t getParticles03um();
+    uint16_t getParticles05um();
+    uint16_t getParticles10um();
+    uint16_t getParticles25um();
+    uint16_t getParticles50um();
+    uint16_t getParticles100um();
+    
+  private:
+    struct pms5003data data;    
+};
+#endif
